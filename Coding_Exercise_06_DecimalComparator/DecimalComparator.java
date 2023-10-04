aa8163ce00f8d91b02ad5be75de052556c6cfc7f
ckbb-4564 <thanadon-boom@outlook.co.th>
2023-10-04 10:15:46 +0700

    :add Exercise 06

---
+++ Coding_Exercise_06_DecimalComparator/DecimalComparator.java
@@ -0,0 +1,5 @@
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double firstNum, double secondNum) {
+        return (int) (firstNum * 1000) == (int) (secondNum * 1000);
+    }
+}
