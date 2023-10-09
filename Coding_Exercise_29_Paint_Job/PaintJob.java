d133b39ab007f8d4d05561eb352a51acccbc553f
ckbb-4564 <thanadon-boom@outlook.co.th>
2023-10-09 15:39:27 +0700

    add Exercise 29

---
+++ Coding_Exercise_29_Paint_Job/PaintJob.java
@@ -0,0 +1,22 @@
+public class PaintJob {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        double area = width * height;
+        int bucketCount = (int) Math.ceil(area / areaPerBucket - extraBuckets);
+        return Math.max(bucketCount, 0);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        return getBucketCount(width, height, areaPerBucket, 0);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        return getBucketCount(1, area, areaPerBucket, 0);
+    }
+}
